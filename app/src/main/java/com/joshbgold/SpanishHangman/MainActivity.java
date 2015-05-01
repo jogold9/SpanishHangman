211a7972e5d5e987b427be08bf062f2cbf5abb7d
joshgoldeneagle <joshuabgold@gmail.com>
2015-04-30 18:54:12 -0700

    improved the logic more

--- app/src/main/java/com/joshbgold/SpanishHangman/MainActivity.java
+++ app/src/main/java/com/joshbgold/SpanishHangman/MainActivity.java
@@ -1,13 +1,14 @@
 package com.joshbgold.SpanishHangman;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends Activity {
 
@@ -16,37 +17,28 @@ public class MainActivity extends Activity {
     private String hiddenWord = "";
     private char hiddenWordChars[];
     private String userGuess ="";
+    boolean match = false;
     private int matchPosition;
-    private int mHits;
-    private int mMisses;
+    private int guessesRemaining = 6;
 
     private TextView someAnswer;
     private Button checkButton;
     private Button exitButton;
     private EditText guessText;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //get the word we are guessing letters for
-    word = wordList.getWord();
 
-    //create a series of dashes or asterisks to represent the number of letters
-    for (int i=0; i < word.length(); i++){
-        hiddenWord += '*';
-    }
         someAnswer = (TextView) findViewById(R.id.answerText);
         checkButton = (Button) findViewById(R.id.submitGuessButton);
         exitButton = (Button) findViewById(R.id.exitButton);
         guessText = (EditText) findViewById(R.id.letterGuess);
 
-        someAnswer.setText(hiddenWord);
 
-        Toast.makeText(MainActivity.this, hiddenWord + " " + word, Toast.LENGTH_LONG).show();
+        playGame();
 
         //check Word button
         View.OnClickListener checkWord = new View.OnClickListener() {
@@ -69,41 +61,119 @@ public class MainActivity extends Activity {
 
    }
 
+    void playGame(){
+        //get the word we are guessing letters for
+        word = wordList.getWord();
+
+        //create a series of dashes or asterisks to represent the number of letters
+        for (int i=0; i < word.length(); i++){
+            hiddenWord += '*';
+        }
+
+        //For testing, will remove this line later
+         Toast.makeText(MainActivity.this, hiddenWord + " " + word, Toast.LENGTH_SHORT).show();
+    }
+
+    void resetVars(){
+        word = "";
+        hiddenWord = "";
+        userGuess ="";
+        match = false;
+        //matchPosition = 0;
+        guessesRemaining = 6;
+    }
+
     private void checkAnswer(){
         //take the user's letter guess
         userGuess = guessText.getText().toString().toLowerCase();
+
+        //checks if user entered a valid letter
+        if (isValidGuess(userGuess)==false){
+            return;
+        }
+
         char[] userGuessChar = userGuess.toCharArray();
 
-        //if there were matches, get index of match(es) locations
-        matchPosition = word.indexOf(userGuess);
+        for (int j = 0; j < word.length(); j++) {
 
-        if (matchPosition != -1){
-            hiddenWordChars = hiddenWord.toCharArray();
-            hiddenWordChars[matchPosition] = userGuessChar[0];
-            hiddenWord = String.valueOf(hiddenWordChars);
-            mHits++;
+            //if there were matches, get index of match(es) locations
+            matchPosition = word.indexOf(userGuess);
 
-            someAnswer.setText(hiddenWord);
+            if (word.indexOf(userGuess) != -1) {
+                hiddenWordChars = hiddenWord.toCharArray();
+                hiddenWordChars[matchPosition] = userGuessChar[0];
+                hiddenWord = String.valueOf(hiddenWordChars);
 
-            Toast.makeText(MainActivity.this, "'" + userGuess + "' is in the mystery word.",
-                    Toast.LENGTH_LONG).show();
+                someAnswer.setText(hiddenWord);
+                match = true;
+            }
+        }
+
+        if (match){
+            Toast.makeText(MainActivity.this, "'" + userGuess + "' is in the mystery word.  You " +
+                    "have " + guessesRemaining + " tries left to solve",
+                    Toast.LENGTH_SHORT).show();
+            match = false;
         }
         else {
-            Toast.makeText(MainActivity.this, "'" + userGuess + "' is not in the mystery word.",
-                    Toast.LENGTH_LONG).show();
-            mMisses++;
+            Toast.makeText(MainActivity.this, "'" + userGuess + "' is not in the mystery " +
+                    "word. You have " + guessesRemaining + " tries left to solve",
+                    Toast.LENGTH_SHORT).show();
+            //decrement the guesses remaining
+            guessesRemaining--;
+
+            checkForLoss(); //checks if user is out of guesses
+
         }
 
+        resetEditText();
         //Display the incorrect letter guesses
-
-        //clear the letter from the edit text area
-        guessText.setHint("");
-        guessText.setText("");
 
         //check if the word is solved, Alert message if the word is solved, with English definition
 
         //When user clicks OK on the alert message, clear all guessed letters, int guesses clears
         //and they get a new mystery verb
     }
+
+    private void resetEditText(){  //clears the letter from EditText area,re-adds hint text
+        guessText.setHint("Type a letter here to guess");
+        guessText.setText("");
+    }
+
+    boolean isValidGuess(String userGuess){
+        if (userGuess.length() == 0){
+            Toast.makeText(MainActivity.this, "You did not enter a letter.  Please type a " +
+                    "letter and press submit button", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    void checkForLoss(){
+        if (guessesRemaining == 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Sorry you ran out of guesses")
+                    .setIcon(R.mipmap.ic_launcher)
+                    .setMessage("The answer was " + word + ".")
+                    .setCancelable(false)
+                    .setNegativeButton("OK",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+            resetVars();
+            playGame();
+
+        }
+        else{
+            return;
+        }
+    }
+
 
 }
